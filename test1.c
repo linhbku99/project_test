80dea8837c8067a7dab952bc7e8d802510f3a4ec
Peter Linh <49335397+linhbku99@users.noreply.github.com>
2019-04-11 09:31:54 +0700

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,60 +1,75 @@
-#include <stdio.h>
+/* Michael Ballantyne Homework 3 CMPT 351 Spring 2012 */
+
 #include <pthread.h>
-static volatile int counter = 0;
-void *mythread(void *arg)
-{
-    printf("%d : begin\n", (int *)arg);
-    int i;
-    for (i = 0; i < 1e7; i++)
-    {
-        counter = counter + 1;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long incircle = 0;
+long points_per_thread;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *runner() {
+    long incircle_thread = 0;
+
+    unsigned int rand_state = rand();
+    long i;
+    for (i = 0; i < points_per_thread; i++) {
+        /* Was initially using random(), but didn't appear to get performance
+         * improvement with threading. random() apparently uses some global state
+         * that is shared between threads, and isn't guaranteed to be threadsafe. */
+        double x = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
+        double y = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
+
+        if (x * x + y * y < 1) {
+            incircle_thread++;
+        }
     }
-    printf("%d : done\n", (int *)arg);
-    return NULL;
+    
+    pthread_mutex_lock(&mutex);
+    incircle += incircle_thread;
+    pthread_mutex_unlock(&mutex);
 }
-int main(int argc, char *argv[])
+
+/* Calculate Pi by the Monte Carlo method. Program arguments are the total number of random
+ * points to use in the calculation and the number of threads to use. */
+int main(int argc, const char *argv[])
 {
-    pthread_t p1, p2;
-    pthread_t p[3];
-    printf("main : begin ( counter = %d)\n", counter);
-    pthread_create(&p1, NULL, mythread, 4);
-    pthread_create(&p2, NULL, mythread, 5);
-    // j o i n waits f o r the threads to f i n i s h
-    pthread_join(p1, NULL);
-    pthread_join(p2, NULL);
-    printf("main : done with both ( counter = %d)\n", counter);
-    for(int i = 0; i < 3; i++)
-    {
-        pthread_create(&p[i], NULL, mythread, i);
-        pthread_join(p[i], NULL);
+    if (argc != 3) {
+        fprintf(stderr, "usage: ./pi <total points> <threads>\n");
+        exit(1);
     }
-    printf("main : done with both ( counter = %d)\n", counter);
+
+    long totalpoints = atol(argv[1]);
+    int thread_count = atoi(argv[2]);
+    points_per_thread = totalpoints / thread_count;
+
+    /* Tried using clock, but it measures CPU time, not wall clock time,
+     * so doesn't demonstrate improvement gained by threading */
+    time_t start = time(NULL);
+
+    srand((unsigned)time(NULL));
+
+    pthread_t *threads = malloc(thread_count * sizeof(pthread_t));
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    int i;
+    for (i = 0; i < thread_count; i++) {
+        pthread_create(&threads[i], &attr, runner, (void *) NULL);
+    }
+
+    for (i = 0; i < thread_count; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    free(threads);
+
+    printf("Pi: %f\n", (4. * (double)incircle) / ((double)points_per_thread * thread_count));
+    printf("Time: %d sec\n", (unsigned int)(time(NULL) - start));
+
     return 0;
 }
-// sua lan 2
-// 3333
-//asd dasd
-// sua lai thu
-// sua de test SSH
-/*
-git config --global user.name "FIRST_NAME LAST_NAME"
-Set your email address: git config --global user.email "MY_NAME@example.com"
-git init
-git status
-git add <filename>
-git commit -m "comment"
-git stash
-git log
-git reset <commit code>
-git reflog
-----------------------------------------------------
-Github
-New SSH key
-New a project
-git remote add origin <link>
-git remote -v
-git push
-git remote rm origin (delete link *)
-
-*/
- // test 1 2 3 4
