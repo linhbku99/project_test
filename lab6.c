6a2789cff25ee90330852d081c8cadd1e79416a8
PL <linh.huynh.bku.99@hcmut.edu.vn>
2019-04-11 09:48:43 +0700

    submit lab6

--- lab6.c
+++ lab6.c
@@ -3,20 +3,26 @@
 #include <stdlib.h>
 #include <time.h>
 
+double incircle = 0;
 long points_per_thread;
-void *runner(void* sum_incircle) {
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void *runner()
+{
     unsigned int rand_state = rand();
-    double* p = (double*) sum_incircle;
     long sum = 0;
-    for (int i = 0; i < points_per_thread; i++) {
+    for (int i = 0; i < points_per_thread; i++)
+    {
         double x = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
         double y = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
-        if (x * x + y * y < 1) {
-        	sum++;
-	}
+        if (x * x + y * y < 1)
+        {
+            sum++;
+        }
     }
-    *p = (double)sum;
-    pthread_exit(NULL);    
+    pthread_mutex_lock(&mutex);
+    incircle += sum;
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
 }
 
 int main(int argc, const char *argv[])
@@ -28,20 +34,17 @@ int main(int argc, const char *argv[])
     srand((unsigned)time(NULL));
     pthread_t threads[4];
     int i;
-    for (i = 0; i < 4; i++) {
-        pthread_create(&threads[i], NULL , runner, (void *)&sum_incircle[i]);
-	//pthread_join(threads[i], NULL);
-    }
-    for (i = 3; i >= 0; i--) {
-      pthread_join(threads[i], NULL);
-    }
-    double incircle = 0;
-    for (int i = 0; i < 4; i++)
+    for (i = 0; i < 4; i++)
     {
-	//printf("%d\n", (int) sum_incircle[i]);
-	incircle += sum_incircle[i];
+        pthread_create(&threads[i], NULL, runner, NULL);
+        //pthread_join(threads[i], NULL);
     }
-    printf("for %d Points & 4 thread Pi = %f\n", (int)totalpoints ,(4. * (double)incircle) / ((double)totalpoints));
-    printf("Time: %f sec\n", (double)(clock() - start)/ CLOCKS_PER_SEC);
+    for (i = 3; i >= 0; i--)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_mutex_destroy(&mutex);
+    printf("for %d Points & 4 thread Pi = %f\n", (int)totalpoints, (4. * (double)incircle) / ((double)totalpoints));
+    printf("Time: %f sec\n", (double)(clock() - start) / CLOCKS_PER_SEC);
     return 0;
 }
